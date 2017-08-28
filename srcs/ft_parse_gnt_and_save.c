3e72a86e61c1df18eaf3c626c7b67882a3ddec28
LineChan <mailine@gmail.com>
2017-08-29 01:16:23 +0200

    ft_parse_gnt_and_save.c

---
+++ srcs/ft_parse_gnt_and_save.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_read_and_save.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvillemi <mvillemi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/29 00:11:15 by mvillemi          #+#    #+#             */
+/*   Updated: 2017/08/29 01:14:15 by mvillemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+/*
+** Read the input and save it in a temporary file
+**
+** 1st parameter : line to be processed
+*/
+int				ft_parse_gnt_and_save(char **line)
+{
+	*line = ft_fgnt("test.txt", '\n');
+	if (!*line)
+		return (0);
+	ft_fprintf(FD,"%s\n", *line);
+	return (1);
+}
