53895b97dd558eb104fe5129ae1f736e1ffd6af3
LineChan <mailine@gmail.com>
2017-09-05 16:17:43 +0200

    ant repartition looking good

--- srcs/ft_ant_repartition.c
+++ srcs/ft_ant_repartition.c
@@ -6,7 +6,7 @@
 /*   By: mvillemi <mvillemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/04 16:20:16 by mvillemi          #+#    #+#             */
-/*   Updated: 2017/09/05 15:00:10 by mvillemi         ###   ########.fr       */
+/*   Updated: 2017/09/05 16:17:13 by mvillemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,69 @@
 /*
 ** Compute ants' repartation on shortest paths
 */
-void			ft_move_ant(t_lst *head, int *ant_ref, const int path_len)
+void			ft_move_ant(t_lst *head, int *finish)
 {
 	t_lst		*it_b;
 	t_lst		*it_a;
 
 	it_b = head->prev;
 	it_a = it_b->prev;
-	ft_fprintf(2, "path_len %d\n", path_len);
+	ft_print_parsing(0, "move_ant", "");
+	if (R_ANT(it_b) != 0)
+		++*finish;
 	while (it_a != head)
 	{
-		ft_fprintf(2, " -%s-    -%s-\n", R_NAME(it_a), R_NAME(it_b));
-		if (R_ANT(it_a))
+		if (R_ANT(it_a) || R_ANT(it_b))
 		{
 			R_ANT(it_b) = R_ANT(it_a);
+			R_ANT(it_a) ^= R_ANT(it_a);
+		ft_fprintf(2, " -%s-[%d]    -%s-[{GREEN:%d}]\n", R_NAME(it_a), R_ANT(it_a), R_NAME(it_b), R_ANT(it_b));
 		}
+		else
+			ft_fprintf(2, " -%s-[%d]    -%s-[%d]\n", R_NAME(it_a), R_ANT(it_a), R_NAME(it_b), R_ANT(it_b));
 		it_b = it_b->prev;
 		it_a = it_a->prev;
 	}
-	++*ant_ref;
+}
 
+void			ft_send_ant(t_lst *head, int *ant_ref)
+{
+	t_lst		*it;
+
+	it = head->next;
+	ft_print_parsing(3, "send_ant", "");
+	R_ANT(it) = *ant_ref;
+	ft_fprintf(2, " %s [{GREEN:%d}]\n", R_NAME(it), R_ANT(it));
+	++*ant_ref;
 }
 
 void			ft_ant_repartition()
 {
 	t_lst		*it;
-	int			ant_ref;
+	int			current_ant;
+	int			finish;
+	int			move;
 
-	ant_ref = 1;
-	while (ant_ref <= ANT_NB)
+	current_ant = 1;
+	finish = 0;
+	move = 0;
+	while (finish != ANT_NB)
 	{
+		ft_printf("finish : %d\n", finish);
 		it = SHORTEST_PATH.next;
 		while (it != &SHORTEST_PATH)
 		{
-			ft_fprintf(2, "  [ {YELLOW:%d} ]\n", PATH_REF(it));
-			ft_move_ant(&C_RESULT(it)->room_head, &ant_ref, PATH_LEN(it));
-			//ft_printf("L%d-%s", ant_ref++, R_NAME(C_RESULT(SHORTEST_PATH.next)->room_head.next));
+			ft_move_ant(&C_RESULT(it)->room_head, &finish);
+			if (current_ant <= ANT_NB)
+			{
+				if ((PATH_LEN(it) <= ((ANT_NB - current_ant) * PATH_LEN(SHORTEST_PATH.next))) || 	(PATH_REF(it) == 0))
+					ft_send_ant(&C_RESULT(it)->room_head, &current_ant);
+			}
 			it = it->next;
+			write(1, "\n", 1);
 		}
-		write(1, "\n", 1);
+		++move;
+		ft_printf("move : %d\n", move);
+		getchar();
 	}
 }
