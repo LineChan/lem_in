0af13b92621d7337d384a54f00d10a2a438e99ca
LineChan <mailine@gmail.com>
2017-09-21 18:03:48 +0200

    output fixed

--- srcs/ft_print_output.c
+++ srcs/ft_print_output.c
@@ -6,7 +6,7 @@
 /*   By: mvillemi <mvillemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/05 18:13:54 by mvillemi          #+#    #+#             */
-/*   Updated: 2017/09/05 21:43:07 by mvillemi         ###   ########.fr       */
+/*   Updated: 2017/09/20 15:58:41 by mvillemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,18 @@ static void			ft_print_room_output(t_lst *head)
 	{
 		if (R_ANT(it))
 			ft_printf("L%d-%s ", R_ANT(it), R_NAME(it));
+			ft_fprintf(FD, "L%d-%s ", R_ANT(it), R_NAME(it));
 		it = it->next;
 	}
 }
 
 void				ft_print_output(void)
-{
+
 	t_lst		*it;
 
 	it = SHORTEST_PATH.next;
 	write(1, "\n", 1);
+	write(FD, "\n", 1);
 	while (it != &SHORTEST_PATH)
 	{
 		ft_print_room_output(&C_RESULT(it)->room_head);
