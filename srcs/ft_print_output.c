19996e964d4e59a1198fd62dd5cce84fb2f3ff71
LineChan <mailine@gmail.com>
2017-09-22 13:14:39 +0200

    reversing output

--- srcs/ft_print_output.c
+++ srcs/ft_print_output.c
@@ -6,7 +6,7 @@
 /*   By: mvillemi <mvillemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/05 18:13:54 by mvillemi          #+#    #+#             */
-/*   Updated: 2017/09/22 11:37:33 by mvillemi         ###   ########.fr       */
+/*   Updated: 2017/09/22 13:14:00 by mvillemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ static void			ft_print_room_output(t_lst *head)
 {
 	t_lst		*it;
 
-	it = head->next;
+	it = head->prev;
 	while (it != head)
 	{
 		if (R_ANT(it))
 			ft_printf("L%d-%s ", R_ANT(it), R_NAME(it));
 			ft_fprintf(FD, "L%d-%s ", R_ANT(it), R_NAME(it));
-		it = it->next;
+		it = it->prev;
 	}
 }
 
